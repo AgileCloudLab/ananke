ad632d3dfe78938e3e3bf2d82d0c7462d0facc55
Lars Nielsen <lnc13.lars@gmail.com>
2021-09-13 17:16:28 +0200

    [:pencil2:] documentation

--- src/ananke/bzip2.hpp
+++ src/ananke/bzip2.hpp
@@ -11,18 +11,35 @@
 
 namespace ananke
 {
-
+///
+/// This is a wrapper for the bzip algorithm 
+///
 class bzip2
 {
 
 public:
 
+    /// Construct the bzip2 object using a json configuration
+    /// @param config is a json object with the appropriate fields for bzip, see second constructor for needed values
+    /// @throws std::runtime_error if a ocnfiguration parameter is missing 
     bzip2(const nlohmann::json& config);
-    
+
+    /// Construct the bzip2 object
+    /// @param block_size that bzip2 will work on
+    /// @param verboisity which bzip2 will use - silent = 0
+    /// @param work_factor used by bzip2 - 30 is the standard value 
     bzip2(const int block_size, const int verbosity, const int work_factor);
 
+    /// Compresses a byte vector
+    /// @param data to be compresed
+    /// @returns a pair of a boolean indicating if any compression happened and the resulting data.
+    ///          if the boolean is false it means data was compressed
     std::pair<bool, std::vector<uint8_t>> compress(std::vector<uint8_t>& data);
 
+    /// Decompress a byte vector
+    /// @param data contains the bytes to be decompressed
+    /// @param size in bytes after compression
+    /// @returns the decompressed byte vector, if input data.size() == size no processing is done and data is returned
     std::vector<uint8_t> decompress(std::vector<uint8_t>& data, const size_t size);
 
     void block_size(const int new_block_size); 
